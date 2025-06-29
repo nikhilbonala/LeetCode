394a0637322855712ed2ff592f27fd7c78192de0
nikhilbonala <123531546+nikhilbonala@users.noreply.github.com>
2025-06-29 14:06:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (89.84%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,34 @@
+from collections import deque
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        g= dict()
+        stack = deque()
+        for e in nums2[::-1]:
+            if not stack:
+                g[e]=-1
+            else:
+                top = stack[-1]
+                if top>e:
+                    g[e] = top
+                else:
+                    while not (top>e):
+                        # print(top,"<",e)
+                        stack.pop()
+                        # print(stack)
+                        if not stack:
+                            top=-1
+                            print(g)
+                            break
+                        else:
+                            top = stack[-1]
+
+                    g[e] = top
+            stack.append(e)
+        result = []
+        for i in nums1:
+            result.append(g[i])
+
+        print(g)
+        # print(result)
+        return result
+
